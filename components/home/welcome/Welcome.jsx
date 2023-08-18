de3884aa43f0bbf42f510eecbed2ec3e620eb516
SeungJin Kim <ksj_1207@hotmail.com>
2023-08-18 11:26:10 +0000

    homepage changes

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import  { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Someone</Text>
+        <Text style={styles.welcomeMessage}>Todays Message</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput styles={styles.searchInput}></TextInput>
+        </View>
+      </View>
     </View>
   )
 }
