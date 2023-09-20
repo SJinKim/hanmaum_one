ce316d8959de63d7c7abfe9e9710b602bf159933
SeungJin Kim <ksj_1207@hotmail.com>
2023-09-20 12:29:02 +0200

    no major changes

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql')
 
 var con = mysql.createConnection({
- host: "",
- user: "",
- password: "",
+  host: '',
+  user: '',
+  password: '',
 })
 
-con.connect(function(err){
- if(err) throw err;
- console.log("Connected")
+con.connect(function (err) {
+  if (err) throw err
+  console.log('Connected')
 })
 
 // pool.query(`select mName from d0391dc4.member`, (err, res) => {
