c5ba79245e722ea6efb54b2491f18633cea99349
SeungJin <ksj_1207@hotmail.com>
2024-06-05 10:59:01 +0200

    Initial Commit

---
+++ Hanmaum_1to1/Hanmaum_1to1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hanmaum_1to1App.swift
+//  Hanmaum_1to1
+//
+//  Created by SeungJin Kim on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Hanmaum_1to1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
