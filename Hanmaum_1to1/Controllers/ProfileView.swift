c0d8ac1d71a220ccd542bd057bcdfc9b82798d55
SeungJin <ksj_1207@hotmail.com>
2024-06-14 19:29:17 +0200

    added picker to profile view

--- Hanmaum_1to1/Controllers/ProfileView.swift
+++ Hanmaum_1to1/Controllers/ProfileView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var password: String = "something"
+    @State var password: String = "something"
+    @State var statusSB = "필수선택"
+    @State var statusSS = "소속 없음"
+    @State var statusS = "믿음"
+    
+    let 성별 = ["필수선택", "남", "여"]
+    let 소속 = ["소속 없음", "장년", "청년"]
+    let 순 = ["믿음", "소망", "사랑", "자비", "양선", "화평", "온유", "희락", "새가족"]
     
     var body: some View {
         NavigationView {
@@ -23,81 +30,87 @@ struct ProfileView: View {
                                 .offset(y: 40)
                         )
                         .padding(.bottom, 30)
-                profile()
+                //Info
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("이름: ")
+                            .bold()
+                        Text("유저1")
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("이메일: ")
+                            .bold()
+                        Text("uandi@hanmaum.de")
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("비밀번호: ")
+                            .bold()
+                        Text("password")
+
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("생년월일: ")
+                            .bold()
+                        Text("1990-01-01")
+
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("휴대폰: ")
+                            .bold()
+                        Text("00000000000")
+
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("성별: ")
+                            .bold()
+                        Picker("성별", selection: $statusSB) {
+                            ForEach(성별, id: \.self){
+                                Text($0)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("소속: ")
+                            .bold()
+                        Picker("소속", selection: $statusSS) {
+                            ForEach(소속, id: \.self){
+                                Text($0)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        Text("순: ")
+                            .bold()
+                        Picker("순", selection: $statusS) {
+                            ForEach(순, id: \.self){
+                                Text($0)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 5)
+                }
+                .padding()
+                
+                Button("정보 수정") {
+                    // change in db
+                    print("adjust personal information")
+                }
+                .padding()
+                .background(Color(red: 0, green: 0, blue: 0.5))
+                .foregroundStyle(.white)
+                .clipShape(Capsule())
             }
         }
     }
 }
-
-@ViewBuilder
-func profile() -> some View {
-    //Info
-    VStack(alignment: .leading) {
-        HStack {
-            Text("Name: ")
-                .bold()
-            Text("User Name")
-        }
-        .padding()
-        HStack {
-            Text("Email: ")
-                .bold()
-            Text("User Email")
-        }
-        .padding()
-        HStack {
-            Text("Password: ")
-                .bold()
-            Text("password")
-            //SecureField("User password", text: $password)
-            //    .textContentType(.password)
-        }
-        .padding()
-        HStack {
-            Text("Birthday: ")
-                .bold()
-            Text("1990-01-01")
-            //SecureField("User password", text: $password)
-            //    .textContentType(.password)
-        }
-        .padding()
-        HStack {
-            Text("Phone: ")
-                .bold()
-            Text("00000000000")
-            //SecureField("User password", text: $password)
-            //    .textContentType(.password)
-        }
-        .padding()
-        HStack {
-            Text("소속: ")
-                .bold()
-            Text("청년")
-            //SecureField("User password", text: $password)
-            //    .textContentType(.password)
-        }
-        .padding()
-        HStack {
-            Text("순: ")
-                .bold()
-            Text("자비")
-            //SecureField("User password", text: $password)
-            //    .textContentType(.password)
-        }
-        .padding()
-    }
-    .padding()
-    
-    Button("정보 수정") {
-        // change in db
-        print("adjust personal information")
-    }
-    .padding()
-    .background(Color(red: 0, green: 0, blue: 0.5))
-    .foregroundStyle(.white)
-    .clipShape(Capsule())
-}
-
+ 
 #Preview {
     ProfileView()
 }
