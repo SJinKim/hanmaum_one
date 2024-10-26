9f233373157e451e4bc463622e3261b88889e8d5
SeungJin <ksj_1207@hotmail.com>
2024-10-26 23:15:04 +0200

    cors added

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,27 @@ const bodyParser = require('body-parser')
 require('dotenv').config
 const port = process.env.NODE_ENV || 3000
 const app = express()
+const domain = 'https://your-frontend-domain.com/'
 
 app.use(bodyParser.json())
+
+// cors as middleware
+app.use(
+  '/one',
+  cors({
+    origin: domain + 'one',
+    methods: ['GET', 'POST', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+)
+app.use(
+  '/admin',
+  cors({
+    origin: domain + 'admin',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+)
 
 // health check endpoint
 app.get('/health', (res, req) => {
