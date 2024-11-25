c93edc148cdac77e3d01f1845081b3c3dcd46495
SeungJin <ksj_1207@hotmail.com>
2024-11-25 21:20:07 +0100

    draft

--- backend/controllers/themeListController.js
+++ backend/controllers/themeListController.js
@@ -1,1 +1,11 @@
 const db = require('../services/db')
+const utils = require('./utilsController')
+
+const getAllTitle = (req, res) => {
+  console.log('get all titles')
+  return utils.getAllValues('weeks', res)
+}
+
+module.exports = {
+  getAllTitle,
+}
